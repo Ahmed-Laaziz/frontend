0075b9dbf02f316defd6c795b28b298d5f31fa55
Laaziz <laazizahmed72@gmail.com>
2023-01-07 14:19:20 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+import CreateMonument from './components/pages/CreateMonument';
+import UpdateMonument from './components/pages/UpdateMonument';
+import ListesMonuments from './components/pages/ListesMonuments';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
+          <Route path="/addmonument" component={CreateMonument} />
+          <Route path="/updateMonument/:id" component={UpdateMonument} exact/>
+                            {/* <Route path="/deleteRegistration/:id" exact component={ListRegistration} /> */}
+          <Route path="/monuments" component={ListesMonuments} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
